2def5b0635a9ea6d26bc2660a6aa6ef278e944c6
NimaKoohkhezri <nimakoohkhezri@gmail.com>
2024-06-05 09:37:03 +0330

    Product.py added

---
+++ Product.py
@@ -0,0 +1,23 @@
+class Product:
+  def __init__(self, name, stock, price):
+    self.name = name
+    self.stock = stock
+    self.price = price
+
+  def get_name(self):
+    return self.name
+
+  def set_name(self, name):
+    self.name = name
+
+  def get_stock(self):
+    return self.stock
+
+  def set_stock(self, stock):
+    self.stock = stock
+
+  def get_price(self):
+    return self.price
+
+  def set_price(self, price):
+    self.price = price
