bbe18c675498835c89d0b357311121341095bcac
NimaKoohkhezri <nimakoohkhezri@gmail.com>
2024-06-05 09:43:53 +0330

    Product.py updated

--- Product.py
+++ Product.py
@@ -1,23 +1,23 @@
 class Product:
-  def __init__(self, name, stock, price):
-    self.name = name
-    self.stock = stock
-    self.price = price
+    def __init__(self, name, stock, price):
+        self.name = name
+        self.stock = stock
+        self.price = price
 
-  def get_name(self):
-    return self.name
+    def get_name(self):
+        return self.name
 
-  def set_name(self, name):
-    self.name = name
+    def set_name(self, name):
+        self.name = name
 
-  def get_stock(self):
-    return self.stock
+    def get_stock(self):
+        return self.stock
 
-  def set_stock(self, stock):
-    self.stock = stock
+    def set_stock(self, stock):
+        self.stock = stock
 
-  def get_price(self):
-    return self.price
+    def get_price(self):
+        return self.price
 
-  def set_price(self, price):
-    self.price = price
+    def set_price(self, price):
+        self.price = price
